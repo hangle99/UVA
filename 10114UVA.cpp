fd436acdcfa7430e3aa0bb9b78be50a3725836d8
Hang Le <lehang2399@gmail.com>
2018-08-11 11:16:23 +0700

    Add new file 10114UVA.cpp

---
+++ 10114UVA.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+int main()
+{
+	int a, d;
+	float b, c;
+	int e;
+	cin>>a>>b>>c>>d>>e;
+	int g= b + c;
+	float gtrixe = g - g* e; 
+	float no = c;
+	int thangkh;
+	float khauhao;
+	vector <float> mangkh;
+
+	for (int i=1; i< d; i++)
+	{
+		cin>>thangkh;
+		cin>>khauhao;
+		for (int j= 1;j<= thangkh; j++ )
+			mangkh.push_back(khauhao);
+	}
+	for (int i = 1; i< d; i++)
+	{
+		if (mang )
+		{
+			no = no -b;
+			gtrixe = gtrixe - gtrixe * 
+		}
+		if (no < gtrixe)
+		{
+			cout<<i<<endl;
+			return 0;
+		}
+	}
+
+}
